4a38ef5b4a3bb10578289118a9641cf9f27f54a8
KAVINESWAR-S <kavineswars770@gmail.com>
2025-09-11 21:49:25 +0530

    post-Sectoin half

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="d-flex vh-100 ">
       <div className='w-20'><Sidebar/></div>
-      <div className='w-50 bg-secondary'><Feed/></div>
+      <div className='w-50'><Feed/></div>
       <div className='w-30'><Suggest/></div>
     </div>
   )
--- src/post.jsx
+++ src/post.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-function post() {
+function Post() {
 
+  const[posts,setPosts] = useState([]);
+
+  useEffect(()=>{
+    
+    fetch('http://localhost:5000/posts')
+    .then((res)=>res.json())
+    .then((data=>setPosts(data)))
+    .catch(err=>console.log(err))
+
+  },[]);
 
     
   return (
-    <div>post</div>
+    <div>
+      {posts.length>0 ?(
+          <div>
+            {posts.map((post)=>(
+              <div key={post.id}>
+                <div className='d-flex'>
+                  <img className="dp rounded-circle"src={post.userImage} alt="profilepic"/>
+                  <h5>{post.username}</h5>
+                </div>
+                <img className='' src={post.postImage} alt="post"/>
+              </div>
+            ))}
+          </div>
+      ):
+        <div>
+          Lodaing posts
+        </div>  
+} 
+      </div>
   )
 }
 
-export default post
+export default Post
